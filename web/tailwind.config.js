28b29dcf71ccf5008350408e6e44d6b6f9e401b1
Oracle Public Cloud User <opc@openclawserver.subnet02142159.vcn02142159.oraclevcn.com>
2026-03-06 01:10:42 +0000

    Finalize web UI styling

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,30 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        forest: {
+          950: '#071c16',
+          900: '#0b2b22',
+          800: '#10362b',
+          700: '#134233',
+          600: '#17503f',
+          100: '#e5f2ed',
+        },
+        gold: {
+          500: '#d8b45c',
+          400: '#e4c77a',
+          300: '#f0db9d',
+        },
+      },
+      boxShadow: {
+        soft: '0 20px 50px -30px rgba(0,0,0,0.45)',
+      },
+      borderRadius: {
+        xl: '1.25rem',
+        '2xl': '1.75rem',
+      },
+    },
+  },
   plugins: [],
 };
